b891f5341a09399fe1d5de28435e3f6c05d08764
dxj <437833417@qq.com>
2017-09-07 21:21:32 +0800

    修改导入导出路径

--- src/com/radar/Config.java
+++ src/com/radar/Config.java
@@ -7,8 +7,8 @@ package com.radar;
  */
 public class Config {
 	public static String seperator = "\\";//文件夹的分隔符，linux下为/,win下为\\
-	public static String dbPath = "jdbc:sqlite:F:\\项目\\雷达系统\\data\\radar";// 数据库存储路径
-	public static String mapPath = "F:\\项目\\雷达系统\\data\\坐标";
+	public static String dbPath = "jdbc:sqlite:resources\\db\\radar";// 数据库存储路径
+	public static String mapPath = "resources\\point";
 	// 地图存储路径,该目录下存储着9个地图数据的分片，分片名称为分片名称为1,2,3,...,9。	
 	public static double radarLong = 116.39164925;// 雷达经度
 	public static double radarLat = 39.78909016;// 雷达纬度
